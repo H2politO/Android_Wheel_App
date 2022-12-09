3abd51b4cf2d237b320905c829a38b203a1cb521
Luca Ferraris <ferraris.luca2001@gmail.com>
2022-12-09 14:56:36 +0100

    Modify IdraActivity to make it full-screen, and add ui components

--- app/src/main/java/com/example/wheelboardapplication/IdraActivity.java
+++ app/src/main/java/com/example/wheelboardapplication/IdraActivity.java
@@ -11,6 +11,8 @@ import android.hardware.usb.UsbManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -57,7 +59,10 @@ public class IdraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_idra);
+
         tv_deviceUsb = findViewById(R.id.usbDevice);
         tv_speed = findViewById(R.id.speed);
         tv_pressure = findViewById(R.id.pressure);
